494917bb3e8e3c5337164bc067281734e9a89337
Janni Tselekoglou <jannitselekoglou@Air-von-Janni.fritz.box>
2022-02-16 22:27:07 +0100

    Readme Update

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This includs Data from the Tokyo Summer Olympics 2020.
 I wanted to learn how to set Data with eacher other in usefull relationship, so that u can Vialize certain Statistik in regards to the Games.
 For example to know which Country has won the Most amount of Goldmedals in this Years Event. 
 
+
+
+
--- pythonfiles/totalmedals.py
+++ pythonfiles/totalmedals.py
@@ -2,16 +2,18 @@ import matplotlib.pyplot as plt, numpy as np
 from data import csvtransfer
 
 k = csvtransfer()
-totalmedals, top_ranked, goldmedals, silvermedals, bronzemedals = k.load_data(5)
+totalmedals, top_ranked, countrys_total, goldmedals, silvermedals, bronzemedals = k.load_data(11)
 
-width = 0.3
+width = 0.6
 fig, ax = plt.subplots()
 ind = np.arange(len(top_ranked))
 
-ax.bar(ind , totalmedals, width)
+totalmedals = sorted(totalmedals)
+dict(sorted(countrys_total.items(), key=lambda item: item[1]))
+ax.barh(ind, totalmedals , width)
 ax.set_xlabel("Country")
 ax.set_ylabel("Total Number")
-ax.set_xticks(ind , top_ranked)
+ax.set_yticks(ind , list(countrys_total))
 
 plt.show()
 
